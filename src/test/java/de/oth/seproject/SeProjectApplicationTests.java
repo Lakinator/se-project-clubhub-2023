9a5e20ee6638248325df978d85316c8fd8d08fcb
Leon Puchinger <leon.puchinger@st.oth-regensburg.de>
2022-10-20 12:06:46 +0200

    initial commit

---
+++ src/test/java/de/oth/seproject/SeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.oth.seproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
