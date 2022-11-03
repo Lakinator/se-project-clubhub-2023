ac75c9dfa8c6aa181515db223b282d95b77c63af
Leon Puchinger <leon.puchinger@st.oth-regensburg.de>
2022-11-03 12:36:46 +0100

    student mapping in controller

--- src/main/java/de/oth/seproject/controller/StudentController.java
+++ src/main/java/de/oth/seproject/controller/StudentController.java
@@ -1,5 +1,20 @@
 package de.oth.seproject.controller;
 
+import de.oth.seproject.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/student")
 public class StudentController {
 
+    @RequestMapping("/add")
+    public ModelAndView showAddStudentForm() {
+        var mv = new ModelAndView();
+        var studentBlank = new Student();
+        mv.addObject("studentForm", studentBlank);
+        mv.setViewName("/student");
+        return null;
+    }
 }
