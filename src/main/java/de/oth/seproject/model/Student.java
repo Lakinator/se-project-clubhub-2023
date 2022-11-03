71ef00c35b89b920d76f6c2a79d598f37a836e11
Leon Puchinger <leon.puchinger@st.oth-regensburg.de>
2022-11-03 12:16:31 +0100

    student getters and setters

--- src/main/java/de/oth/seproject/model/Student.java
+++ src/main/java/de/oth/seproject/model/Student.java
@@ -1,10 +1,37 @@
 package de.oth.seproject.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "student")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
 
+    String name, email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
