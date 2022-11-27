150307ece524f542146c13b5c00dc8737fa863e7
Lukas Schalk <lukas.schalk@st.oth-regensburg.de>
2022-11-28 00:14:40 +0100

    Added redirect to registration

--- src/main/java/de/oth/seproject/clubhub/web/RegistrationController.java
+++ src/main/java/de/oth/seproject/clubhub/web/RegistrationController.java
@@ -8,14 +8,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -46,7 +42,7 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public ModelAndView registerUser(@ModelAttribute("user") @Valid final User user, BindingResult result, Model model) {
+    public String registerUser(@ModelAttribute("user") @Valid final User user, BindingResult result, Model model) {
 
         // TODO: user validation
 
@@ -57,7 +53,7 @@ public class RegistrationController {
 
         userRepository.save(user);
 
-        return new ModelAndView("login"); // TODO: show successful register page
+        return "redirect:/login"; // TODO: show successful register page
     }
 
 }
