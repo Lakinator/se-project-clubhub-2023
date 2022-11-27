e7f3be24c9805277c9681a2197d8f4c89bd6fdbf
Lukas Schalk <lukas.schalk@st.oth-regensburg.de>
2022-11-27 23:25:04 +0100

    Added club selection to registration

--- src/main/java/de/oth/seproject/clubhub/web/RegistrationController.java
+++ src/main/java/de/oth/seproject/clubhub/web/RegistrationController.java
@@ -1,18 +1,23 @@
 package de.oth.seproject.clubhub.web;
 
+import de.oth.seproject.clubhub.persistence.model.Club;
 import de.oth.seproject.clubhub.persistence.model.User;
+import de.oth.seproject.clubhub.persistence.repository.ClubRepository;
 import de.oth.seproject.clubhub.persistence.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class RegistrationController {
@@ -21,20 +26,27 @@ public class RegistrationController {
 
     private final UserRepository userRepository;
 
-    public RegistrationController(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    private final ClubRepository clubRepository;
+
+    public RegistrationController(PasswordEncoder passwordEncoder, UserRepository userRepository, ClubRepository clubRepository) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
+        this.clubRepository = clubRepository;
     }
 
     @GetMapping("/registration")
     public String registrationPage(Model model) {
         User user = new User();
         model.addAttribute("user", user);
+
+        List<Club> clubs = clubRepository.findAll();
+        model.addAttribute("clubs", clubs);
+
         return "registration";
     }
 
     @PostMapping("/registration")
-    public ModelAndView registerUser(@ModelAttribute("user") @Valid final User user, final HttpServletRequest request, final Errors errors) {
+    public ModelAndView registerUser(@ModelAttribute("user") @Valid final User user, BindingResult result, Model model) {
 
         // TODO: user validation
 
