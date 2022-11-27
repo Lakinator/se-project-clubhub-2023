67f91b95a5f9f447e798b1a4672a15e0662ef9f0
Lukas Schalk <lukas.schalk@st.oth-regensburg.de>
2022-11-27 19:09:47 +0100

    Added user login

--- src/main/java/de/oth/seproject/clubhub/web/HomeController.java
+++ src/main/java/de/oth/seproject/clubhub/web/HomeController.java
@@ -1,6 +1,8 @@
 package de.oth.seproject.clubhub.web;
 
+import de.oth.seproject.clubhub.config.ClubUserDetails;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     @Value("${spring.application.name}")
-    String appName;
+    private String appName;
 
     @GetMapping("/home")
-    public String homePage(Model model) {
+    public String homePage(@AuthenticationPrincipal ClubUserDetails userDetails, Model model) {
         model.addAttribute("appName", appName);
         return "home";
     }
