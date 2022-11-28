0e9f8b4a7ef5c0a9dc46abf23e70f8575bd4c78f
Lukas Schalk <lukas.schalk@st.oth-regensburg.de>
2022-11-28 18:39:07 +0100

    Added pagination to groups

--- src/main/java/de/oth/seproject/clubhub/persistence/repository/GroupRepository.java
+++ src/main/java/de/oth/seproject/clubhub/persistence/repository/GroupRepository.java
@@ -2,6 +2,8 @@ package de.oth.seproject.clubhub.persistence.repository;
 
 import de.oth.seproject.clubhub.persistence.model.Club;
 import de.oth.seproject.clubhub.persistence.model.Group;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +11,7 @@ import java.util.List;
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
     List<Group> findAllByClub(Club club);
+
+    Page<Group> findAllByClub(Club club, Pageable pageable);
 
 }
