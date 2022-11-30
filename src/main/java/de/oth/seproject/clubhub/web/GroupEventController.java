050719f0df0fe112a196e118c5a97b56a335746c
Lukas Schalk <lukas.schalk@st.oth-regensburg.de>
2022-11-30 19:23:16 +0100

    Added basic group event creation for trainers of a group

--- src/main/java/de/oth/seproject/clubhub/web/GroupEventController.java
+++ src/main/java/de/oth/seproject/clubhub/web/GroupEventController.java
@@ -1,19 +1,18 @@
 package de.oth.seproject.clubhub.web;
 
 import de.oth.seproject.clubhub.config.ClubUserDetails;
-import de.oth.seproject.clubhub.persistence.model.Group;
-import de.oth.seproject.clubhub.persistence.model.GroupEvent;
-import de.oth.seproject.clubhub.persistence.model.Role;
+import de.oth.seproject.clubhub.persistence.model.*;
 import de.oth.seproject.clubhub.persistence.repository.*;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -42,8 +41,8 @@ public class GroupEventController {
     @GetMapping("/group/{groupId}/calendar")
     public String showGroupPage(@AuthenticationPrincipal ClubUserDetails userDetails, @PathVariable("groupId") long groupId, @RequestParam("intervalStart") Optional<LocalDate> intervalStart,
                                 @RequestParam("intervalEnd") Optional<LocalDate> intervalEnd, Model model) {
-        LocalDate currentIntervalStart = intervalStart.orElse(LocalDate.now().plusDays(7));
-        LocalDate currentIntervalEnd = intervalEnd.orElse(LocalDate.now());
+        LocalDate currentIntervalStart = intervalStart.orElse(LocalDate.now().minusDays(7));
+        LocalDate currentIntervalEnd = intervalEnd.orElse(LocalDate.now().plusDays(7));
 
         Group group = groupRepository.findById(groupId)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid group Id:" + groupId));
@@ -53,6 +52,39 @@ public class GroupEventController {
         model.addAttribute("group", group);
         model.addAttribute("groupEvents", groupEvents);
         return "show-group-calendar";
+    }
+
+    @GetMapping("/group/{groupId}/add-event")
+    public String addGroupEventPage(@AuthenticationPrincipal ClubUserDetails userDetails, @PathVariable("groupId") long groupId, Model model) {
+        Group group = groupRepository.findById(groupId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid group Id:" + groupId));
+
+        GroupEvent groupEvent = new GroupEvent();
+        model.addAttribute("groupEvent", groupEvent);
+        model.addAttribute("group", group);
+        model.addAttribute("eventTypes", EventType.values());
+        return "add-group-event";
+    }
+
+    @PostMapping("/group/{groupId}/create-event")
+    public String createGroupEvent(@AuthenticationPrincipal ClubUserDetails userDetails, @PathVariable("groupId") long groupId, @Valid GroupEvent groupEvent, BindingResult result, Model model) {
+        Group group = groupRepository.findById(groupId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid group Id:" + groupId));
+
+        // TODO: validation
+
+        Optional<Role> roleInGroup = roleRepository.findByUserAndGroup(userDetails.getUser(), group);
+        final boolean isTrainerInGroup = roleInGroup.isPresent() && roleInGroup.get().getAuthority().equals(RoleType.TRAINER.name());
+
+        if (isTrainerInGroup) {
+            groupEvent.setGroup(group);
+            groupEvent.setUser(roleInGroup.get().getUser());
+
+            groupEventRepository.save(groupEvent);
+        }
+
+
+        return "redirect:/group/" + groupId + "/calendar";
     }
 
 }
