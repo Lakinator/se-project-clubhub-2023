eb2c81653798a453bf933a9e622bd1966e7508bc
Lukas Schalk <lukas.schalk@st.oth-regensburg.de>
2022-12-01 19:37:47 +0100

    Updated role repository

--- src/main/java/de/oth/seproject/clubhub/persistence/repository/RoleRepository.java
+++ src/main/java/de/oth/seproject/clubhub/persistence/repository/RoleRepository.java
@@ -12,16 +12,41 @@ import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Optional<Role> findByUserAndGroup(User user, Group group);
-
-    Boolean existsByUserAndGroup(User user, Group group);
-
     Page<Role> findAllByGroup(Group group, Pageable pageable);
 
+    /**
+     * @param user  -
+     * @param group -
+     * @return The current role of a user in the given group or {@link Optional#empty()} if there is no role
+     */
+    Optional<Role> findByUserAndGroup(User user, Group group);
+
+    /**
+     * @param user  -
+     * @param group -
+     * @return Whether the user has a role in this group
+     */
+    Boolean existsByUserAndGroup(User user, Group group);
+
+    /**
+     * @param group -
+     * @return Whether the group has any roles. If false, there are no members in this group
+     */
     Boolean existsAllByGroup(Group group);
 
-    Boolean existsByGroupAndRoleName(Group group, RoleType roleType);
-
+    /**
+     * @param user     -
+     * @param roleType -
+     * @return Whether the user has this role in any group
+     */
     Boolean existsByUserAndRoleName(User user, RoleType roleType);
+
+    /**
+     * @param user     -
+     * @param group    -
+     * @param roleType -
+     * @return Whether the user has this role in a specific group
+     */
+    Boolean existsByUserAndGroupAndRoleName(User user, Group group, RoleType roleType);
 
 }
