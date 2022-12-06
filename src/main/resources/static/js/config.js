46af8e48e2812e7fef3a45034ceb4923a91c5542
Lukas Schalk <lukas.schalk@st.oth-regensburg.de>
2022-12-06 23:21:35 +0100

    Replaced external resource links with webjars

---
+++ src/main/resources/static/js/config.js
@@ -0,0 +1,6 @@
+function enable_tooltips() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+}
+
+window.onload = enable_tooltips;
